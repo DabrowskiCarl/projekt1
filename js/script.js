98d68f7e103fe23c639d561cfddd6c9c2e64dae8
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-04-19 00:03:10 +0200

    Add files, folders, sctructure, basic scripts

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+    const $secondrow = $('#secondrow');
+    const $shbutton = $('#showhide');
+    const $angle = $('#angle');
+    $secondrow.hide();
+    $shbutton.click(function(){
+        const text = $shbutton.text();
+        if (text === 'Show more') {
+            $shbutton.text('Hide');
+        }
+        else {
+            $shbutton.text('Show more');
+        }
+        $angle.toggleClass('fa-angle-down');
+        $angle.toggleClass('fa-angle-up');
+        $secondrow.toggle(500);
+    });
+
+    $('#firstModal').on('hidden.bs.modal', function () {
+        $('#firstmdlbtn').text('Thank You');
+    })
+});
+
